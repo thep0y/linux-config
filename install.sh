02d9c7dc6996a923eae8507e210f1d1cf6d850ad
thepoy <thepoy@163.com>
2021-12-30 20:26:01 +0800

    linuxmint 添加到 ubuntu 判断代码块中

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # @Author: thepoy
 # @Date:   2021-12-30 19:08:33
 # @Last Modified by:   thepoy
-# @Last Modified time: 2021-12-30 20:15:37
+# @Last Modified time: 2021-12-30 20:25:27
 
 set -eux
 
@@ -13,7 +13,7 @@ if [ "$ID" = "arch" ]; then
     install_cmd="sudo pacman -S --noconfirm "
     # 配置 pacman 源
     sudo pacman -Syy
-elif [ "$ID" = "ubuntu" ]; then
+elif [ "$ID" = "ubuntu" ] || [ "$ID" = "linuxmint" ]; then
     install_cmd="sudo apt install -y "
     # 配置 ubuntu 源
     $VERSION_CODENAME
