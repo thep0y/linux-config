8946a932f754b9abc63f714624b4631b61891edb
thepoy <thepoy@163.com>
2021-12-30 21:50:54 +0800

    安装一个必要包

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # @Author: thepoy
 # @Date:   2021-12-30 19:08:33
 # @Last Modified by:   thepoy
-# @Last Modified time: 2021-12-30 21:49:36
+# @Last Modified time: 2021-12-30 21:50:47
 
 set -eux
 
@@ -68,7 +68,7 @@ hostname='github.com.cnpmjs.org'
 port=443
 trust_cert_file_location=`curl-config --ca`
 
-${install_cmd}libcurl-dev
+${install_cmd}libcurl4-gnutls-dev
 
 sudo bash -c "echo -n | openssl s_client -showcerts -connect $hostname:$port -servername $hostname \
     2>/dev/null  | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'  \
