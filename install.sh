9729c476dcabe5235bbdf332c40696f24d0e1523
thepoy <thepoy@163.com>
2021-12-30 20:16:48 +0800

    ubuntu可能有第三方发行版，所以应该使用`UBUNTU_CODENAME`确定 ubuntu 版本代号

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # @Author: thepoy
 # @Date:   2021-12-30 19:08:33
 # @Last Modified by:   thepoy
-# @Last Modified time: 2021-12-30 20:04:48
+# @Last Modified time: 2021-12-30 20:15:37
 
 set -eux
 
@@ -17,6 +17,7 @@ elif [ "$ID" = "ubuntu" ]; then
     install_cmd="sudo apt install -y "
     # 配置 ubuntu 源
     $VERSION_CODENAME
+    $UBUNTU_CODENAME
     sudo apt update
 elif [ "$ID" = "debian" ]; then
     install_cmd="sudo apt install -y "
