0f7865fa3a97d35285248a69ce0b74f0338593bb
thepoy <thepoy@163.com>
2021-12-30 20:27:20 +0800

    删除 ubuntu 语句块中的 VERSION_CODENAME

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # @Author: thepoy
 # @Date:   2021-12-30 19:08:33
 # @Last Modified by:   thepoy
-# @Last Modified time: 2021-12-30 20:25:27
+# @Last Modified time: 2021-12-30 20:26:43
 
 set -eux
 
@@ -16,7 +16,6 @@ if [ "$ID" = "arch" ]; then
 elif [ "$ID" = "ubuntu" ] || [ "$ID" = "linuxmint" ]; then
     install_cmd="sudo apt install -y "
     # 配置 ubuntu 源
-    $VERSION_CODENAME
     $UBUNTU_CODENAME
     sudo apt update
 elif [ "$ID" = "debian" ]; then
