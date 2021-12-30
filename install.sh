a0adb1cf416b4bb97486665159efcdcaadf6f6ec
thepoy <thepoy@163.com>
2021-12-30 20:29:05 +0800

    mint和ubuntu的源文件不一样，需要分别处理

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # @Author: thepoy
 # @Date:   2021-12-30 19:08:33
 # @Last Modified by:   thepoy
-# @Last Modified time: 2021-12-30 20:26:43
+# @Last Modified time: 2021-12-30 20:28:34
 
 set -eux
 
@@ -13,7 +13,12 @@ if [ "$ID" = "arch" ]; then
     install_cmd="sudo pacman -S --noconfirm "
     # 配置 pacman 源
     sudo pacman -Syy
-elif [ "$ID" = "ubuntu" ] || [ "$ID" = "linuxmint" ]; then
+elif [ "$ID" = "ubuntu" ]; then
+    install_cmd="sudo apt install -y "
+    # 配置 ubuntu 源
+    $VERSION_CODENAME
+    sudo apt update
+elif [ "$ID" = "linuxmint" ]; then
     install_cmd="sudo apt install -y "
     # 配置 ubuntu 源
     $UBUNTU_CODENAME
